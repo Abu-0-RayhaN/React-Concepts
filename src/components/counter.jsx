b1c837dd1e167343aad45daa336833b667fa67c5
Abu-0-RayhaN <rayhan844625@gmail.com>
2023-03-23 23:12:24 +0600

    nothing

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 class Counter extends Component { 
     state ={ 
         count:0,
-        tags:['tag1','tag2','tag3']
+        tags:[]
     };
     renderTags(){
         if (this.state.tags.length === 0) return <p>There are no tags!</p>;
