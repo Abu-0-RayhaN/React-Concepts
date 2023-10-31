c1d0599e5fe5b1cbafc1141bd08b6c62efcf0e37
Abu-0-RayhaN <rayhan844625@gmail.com>
2023-10-31 22:14:43 +0600

    Route defining

--- src/index.js
+++ src/index.js
@@ -1,9 +1,29 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "bootstrap/dist/css/bootstrap.css";
-
+import {
+  Route,
+  RouterProvider,
+  createBrowserRouter,
+  createRoutesFromElements,
+} from "react-router-dom";
+import Layout from "./router-practice/Layout";
+import Home from "./router-practice/Home";
+import About from "./router-practice/AboutUs";
+import Contact from "./router-practice/ContactUs";
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<Layout />}>
+      <Route path="" element={<Home />} />
+      <Route path="about" element={<About />} />
+      <Route path="contact" element={<Contact />} />
+    </Route>
+  )
+);
 ReactDOM.render(
-  <></>,
+  <>
+    <RouterProvider router={router} />
+  </>,
 
   document.getElementById("root")
 );
--- src/router-practice/Header.jsx
+++ src/router-practice/Header.jsx
@@ -1,69 +1,93 @@
-// import { Link, NavLink } from "react-router-dom";
+import { Link, NavLink } from "react-router-dom";
 
-// export default function Header() {
-//   return (
-//     <header className="shadow sticky-top">
-//       <nav className="navbar navbar-expand-lg navbar-light bg-white border">
-//         <div className="container">
-//           <Link to="/" className="navbar-brand">
-//             <img
-//               src="https://alexharkness.com/wp-content/uploads/2020/06/logo-2.png"
-//               className="mr-3 h-12"
-//               alt="Logo"
-//             />
-//           </Link>
-//           <button
-//             className="navbar-toggler"
-//             type="button"
-//             data-bs-toggle="collapse"
-//             data-bs-target="#navbarNav"
-//             aria-controls="navbarNav"
-//             aria-expanded="false"
-//             aria-label="Toggle navigation"
-//           >
-//             <span className="navbar-toggler-icon"></span>
-//           </button>
-//           <div
-//             className="collapse navbar-collapse justify-content-end"
-//             id="navbarNav"
-//           >
-//             <ul className="navbar-nav">
-//               <li className="nav-item">
-//                 <NavLink
-//                   to="#"
-//                   className="nav-link text-gray-800 border-bottom border-2 border-transparent px-3 py-2.5 me-2"
-//                   activeClassName="border-orange-700 text-orange-700"
-//                 >
-//                   Home
-//                 </NavLink>
-//               </li>
-//               <li className="nav-item">
-//                 <Link
-//                   to="#"
-//                   className="nav-link text-gray-800 border-bottom border-2 border-transparent px-3 py-2.5 me-2"
-//                 >
-//                   Log in
-//                 </Link>
-//               </li>
-//               <li className="nav-item">
-//                 <Link
-//                   to="#"
-//                   className="nav-link text-white bg-orange-700 hover:bg-orange-800 rounded px-4 py-2.5 me-2"
-//                 >
-//                   Get started
-//                 </Link>
-//               </li>
-//             </ul>
-//           </div>
-//         </div>
-//       </nav>
-//     </header>
-//   );
-// }
-import React from "react";
+export default function Header() {
+  return (
+    <header className="shadow sticky-top">
+      <nav className="navbar navbar-expand-lg navbar-light bg-white border">
+        <div className="container">
+          <Link to="/" className="navbar-brand">
+            <img
+              src="https://alexharkness.com/wp-content/uploads/2020/06/logo-2.png"
+              className="mr-3 h-12"
+              alt="Logo"
+              height={50}
+              width={100}
+            />
+          </Link>
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarNav"
+            aria-controls="navbarNav"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div
+            className="collapse navbar-collapse justify-content-end"
+            id="navbarNav"
+          >
+            <ul className="navbar-nav">
+              <li className="nav-item">
+                <NavLink
+                  to="/"
+                  className="nav-link text-gray-800 border-bottom border-2 border-transparent px-3 py-2.5 me-2"
+                  activeClassName="border-orange-700 text-orange-700"
+                >
+                  Home
+                </NavLink>
+              </li>
+              <li className="nav-item">
+                <NavLink
+                  to="/about"
+                  className="nav-link text-gray-800 border-bottom border-2 border-transparent px-3 py-2.5 me-2"
+                  activeClassName="border-orange-700 text-orange-700"
+                >
+                  About
+                </NavLink>
+              </li>
+              <li className="nav-item">
+                <NavLink
+                  to="/contact"
+                  className="nav-link text-gray-800 border-bottom border-2 border-transparent px-3 py-2.5 me-2"
+                  activeClassName="border-orange-700 text-orange-700"
+                >
+                  Contact
+                </NavLink>
+              </li>
+              <li className="nav-item">
+                <Link
+                  to="/login"
+                  className="nav-link text-gray-800 border-bottom border-2 border-transparent px-3 py-2.5 me-2"
+                >
+                  Log in
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link
+                  to="/GetStarted"
+                  className="nav-link text-primary border rounded px-4 py-2.5 me-2"
+                >
+                  Get started
+                </Link>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+    </header>
+  );
+}
+// import React from "react";
 
-const Header = () => {
-  return <div>Header</div>;
-};
+// const Header = () => {
+//   return <>
+//   <header className="">
 
-export default Header;
+//   </header>
+//   </>;
+// };
+
+// export default Header;
